46ae6dbd7c98947f7451cfeffaa6af02f65efb66
Nikita <zharinov.nicita@gmail.com>
2022-06-28 10:53:48 +0700

    [feature/service-cdn] #12

--- src/cdn/cdn.controller.ts
+++ src/cdn/cdn.controller.ts
@@ -2,8 +2,7 @@ import { Response, Request } from 'express'
 
 type QueryType = {
   _id: string
-  type: string
-  name: string
+  type: 'aritst' | 'album' | 'track'
 }
 
 type FilesType = {
@@ -13,16 +12,16 @@ type FilesType = {
 class CdnController {
   constructor() {}
   async upload(req: Request, res: Response) {
-    if (req?.query && req?.files) {
-      if (
-        req.query?._id &&
-        req.query?.type &&
-        req.query?.name &&
-        req.files?.picture
-      ) {
-        const { _id, name, type }: QueryType = Object(req.query)
+    const isQueryAndIsFiles = req?.query && req?.files
+
+    if (isQueryAndIsFiles) {
+      const isQueryAndIsFilesProperty =
+        req.query?._id && req.query?.type && req.files?.picture
+
+      if (isQueryAndIsFilesProperty) {
+        const { _id, type }: QueryType = Object(req.query)
         const { picture }: FilesType = Object(req.files)
-        return res.json({ _id, type, name, picture: picture.name })
+        return res.json({ _id, type, picture: picture.name })
       }
       return res.json('error')
     }
