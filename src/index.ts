0dbcbe6540f8cb9cad5f8f1285663e320070a4b5
Nikita <zharinov.nicita@gmail.com>
2022-06-29 13:43:57 +0700

    [develop] .env добавил в .gitingnore

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import fileUpload from 'express-fileupload'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(cors())
